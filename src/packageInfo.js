16e71d0dfe911937f1a235bfd350a2882162fb26
Omar Gamal <omrgamo1@gmail.com>
2026-09-03 18:37:40 +0300

    fix: src/packageInfo.js

--- src/packageInfo.js
+++ src/packageInfo.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
+import path from 'node:path';
 
-const packageInfo = JSON.parse(await fs.promises.readFile('../package.json'));
+const packageInfo = JSON.parse(await fs.promises.readFile(path.join(import.meta.dirname, '../package.json')));
 
 export { packageInfo };
